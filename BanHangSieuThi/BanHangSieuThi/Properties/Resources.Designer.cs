12ed22aa039800f4a0f74661e512232658694534
tanphatkyhoa@gmail.com <tanphatkyhoa@gmail.com>
2019-10-16 21:13:55 +0700

    form dang nhap

--- BanHangSieuThi/BanHangSieuThi/Properties/Resources.Designer.cs
+++ BanHangSieuThi/BanHangSieuThi/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace BanHangSieuThi.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap background1 {
+            get {
+                object obj = ResourceManager.GetObject("background1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap home {
             get {
                 object obj = ResourceManager.GetObject("home", resourceCulture);
