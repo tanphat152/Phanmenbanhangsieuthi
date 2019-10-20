499159c820efba230ba20cb6c929ecbdd7def9be
huytra1998 <54475092+huytra1998@users.noreply.github.com>
2019-10-20 17:54:30 +0700

    chuoi connect

---
+++ BanHangSieuThi/BanHangSieuThi/DataAccess/ConnectString.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace BanHangSieuThi.DataAccess
+{
+    class ConnectString
+    {
+        public string getConnectionString(int value)
+        {
+            if (value == 0)
+            {
+                return @"Data Source=localhost;Initial Catalog=BanHangSieuThi;Integrated Security=True";
+            }
+            else
+            {
+                return @"Data Source=" + Environment.MachineName + "\\SQLEXPRESS;Initial Catalog=BanHangSieuThi;Integrated Security=True";
+            }
+        }
+        public int checkCnn()
+        {
+            try
+            {
+                string temp = @"Data Source=" + Environment.MachineName + "\\SQLEXPRESS;Initial Catalog=BanHangSieuThi;Integrated Security=True";
+                using (SqlConnection connect = new SqlConnection(temp))
+                {
+
+                    connect.Open();
+                    connect.Close();
+                }
+                return 1;
+            }
+            catch
+            {
+
+                string temp = @"Data Source=localhost;Initial Catalog=BanHangSieuThi;Integrated Security=True";
+                using (SqlConnection connect = new SqlConnection(temp))
+                {
+
+                    connect.Open();
+                    connect.Close();
+                }
+                return 0;
+            }
+
+        }
+    }
+}
