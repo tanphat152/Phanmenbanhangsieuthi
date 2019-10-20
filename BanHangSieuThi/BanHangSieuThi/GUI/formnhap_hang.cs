d429506d57b46c35b11488b8f96fe3e13c5b4e15
huytra1998 <54475092+huytra1998@users.noreply.github.com>
2019-10-20 18:50:13 +0700

    them hang

--- BanHangSieuThi/BanHangSieuThi/GUI/formnhap_hang.cs
+++ BanHangSieuThi/BanHangSieuThi/GUI/formnhap_hang.cs
@@ -19,15 +19,50 @@ namespace BanHangSieuThi.GUI
         {
             InitializeComponent();
         }
-        
+        SqlConnection connection;
+        SqlCommand cmd;
+        ConnectString connectStr = new ConnectString();
+        SqlDataAdapter adap = new SqlDataAdapter();
+        DataTable table = new DataTable();
+        void LoadData()
+        {
+            cmd = connection.CreateCommand();
+            cmd.CommandText = "SELECT MaSP,TenSP,TenLH,SoLuong,LoiNhuan,GiaNhap,GiaBan,MoTa,NSX FROM dbo.tblSanPham,dbo.tblLoaiHang WHERE tblLoaiHang.MaLH=tblSanPham.MaLH";
+            adap.SelectCommand = cmd;
+            table.Clear();
+            adap.Fill(table);
+            gunaDataGridView1.DataSource = table;
+
+        }
         private void formnhap_hang_Load(object sender, EventArgs e)
         {
-            
+            using (connection = new SqlConnection(connectStr.getConnectionString(1)))
+            {
+                connection.Open();
+                LoadData();
+                connection.Close();
+            }
         }
 
         private void gunaButton2_Click(object sender, EventArgs e)
         {
-
+            using(connection = new SqlConnection(connectStr.getConnectionString(1)))
+            {
+                connection.Open();
+                cmd = connection.CreateCommand();
+                cmd.CommandText = "INSERT INTO dbo.tblSanPham VALUES  ( '"+guTbMaSP.Text+"' , N'"+guTbTenSP.Text+ "' , 'LH5' , "+guTbSLSP.Text+" , '"+guTbLoiNhuanSP.Text+"' , '"+guTbGiaNhapSP.Text+"' , '"+guTbGiaBanSP.Text+"' , N'"+guTbMoTa.Text+"' , N'"+guTbNXS.Text+ "' , N'' , N'LaptopVip' )";
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("thêm thành công");
+                }
+                catch
+                {
+                    MessageBox.Show("Thêm không thành công");
+                }
+                LoadData();
+                connection.Close();
+            }
         }
     }
 }
