4cf13389ea0928fbc73ec0104009c59a86fc05bf
soncutemta1234 <ngocsonqh1@gmail.com>
2019-10-21 20:20:30 +0700

    change code

--- BanHangSieuThi/BanHangSieuThi/GUI/frmAddNhanvien.cs
+++ BanHangSieuThi/BanHangSieuThi/GUI/frmAddNhanvien.cs
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 namespace BanHangSieuThi.GUI
 {
     public partial class frmAddnhanvien : Form
-    {
+    {    
         public frmAddnhanvien()
         {
             InitializeComponent();   
