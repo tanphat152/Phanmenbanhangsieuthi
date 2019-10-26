d7e5a6b422f513fd37795f992ee0aeb825f92425
khanhtoan99gvc <khanhtoan99gvc@gmail.com>
2019-10-26 09:05:51 +0700

    thong ke hang hoa trong kho

--- BanHangSieuThi/BanHangSieuThi/GUI/thongke.Designer.cs
+++ BanHangSieuThi/BanHangSieuThi/GUI/thongke.Designer.cs
@@ -36,10 +36,10 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 87);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 74);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(776, 351);
+            this.dataGridView1.Size = new System.Drawing.Size(776, 364);
             this.dataGridView1.TabIndex = 0;
             // 
             // label1
